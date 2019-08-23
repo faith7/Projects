ffa09e918f8c5d728bf40388bdbb8b079bb9c035
faith7 <faithdevelop@gmail.com>
2019-08-23 00:56:08 -0500

    Feat: add flash messages

--- Project2_Catalog/app.py
+++ Project2_Catalog/app.py
@@ -153,7 +153,7 @@ def editCategory(category_id):
             editCat.genre = request.form['genre']
         session.add(editCat)
         session.commit()
-        flash("You just updated the category!")
+        flash("You just updated a category!")
         return redirect(url_for('showCategory', category_id=category_id))
     else:
         return render_template('editCategory.html',
@@ -193,7 +193,7 @@ def delItem(category_id, item_id):
 @app.route('/catalog/<int:category_id>/<int:item_id>/edit/',
            methods=['GET', 'POST'])
 def editItem(category_id, item_id):
-    # categories = session.query(Category).all()
+    categories = session.query(Category).all()
     category = session.query(Category).filter_by(id=category_id).one()
     editItem = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
@@ -238,6 +238,6 @@ def editItem(category_id, item_id):
 
 
 if __name__ == '__main__':
-    app.debug = True
+    # app.debug = True
     app.secret_key = 'super_secret_key'
     app.run(host='0.0.0.0', port=8080)
